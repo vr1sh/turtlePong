0aba36010ffa080bbc94586bbc656a10464f4c99
Vrishank Mishra <42439148+vr1sh@users.noreply.github.com>
2020-08-05 22:40:48 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,121 @@
+import turtle
+
+window = turtle.Screen()
+window.title("Pong")
+window.bgcolor("black")
+window.setup(width=800, height=600)
+window.tracer(0)
+
+# paddle 1
+paddle_1 = turtle.Turtle()
+paddle_1.speed(0)
+paddle_1.shape("square")
+paddle_1.shapesize(stretch_wid=5, stretch_len=1)
+paddle_1.color("white")
+paddle_1.penup()
+paddle_1.goto(-350, 0)
+
+
+def paddle_1_up():
+    y = paddle_1.ycor()
+    y += 20
+    paddle_1.sety(y)
+
+
+def paddle_1_down():
+    y = paddle_1.ycor()
+    y -= 20
+    paddle_1.sety(y)
+
+
+# paddle 2
+paddle_2 = turtle.Turtle()
+paddle_2.speed(0)
+paddle_2.shape("square")
+paddle_2.shapesize(stretch_wid=5, stretch_len=1)
+paddle_2.color("white")
+paddle_2.penup()
+paddle_2.goto(350, 0)
+
+
+def paddle_2_up():
+    y = paddle_2.ycor()
+    y += 20
+    paddle_2.sety(y)
+
+
+def paddle_2_down():
+    y = paddle_2.ycor()
+    y -= 20
+    paddle_2.sety(y)
+
+
+# ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 0.4
+ball.dy = 0.4
+
+# pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player 1: 0 player 2: 0", align="center",
+          font=("Courier", 24, "normal"))
+
+# score
+score_1 = 0
+score_2 = 0
+
+
+# key bindings
+window.listen()
+window.onkeypress(paddle_1_up, "w")
+window.onkeypress(paddle_1_down, "s")
+window.onkeypress(paddle_2_up, "Up")
+window.onkeypress(paddle_2_down, "Down")
+
+
+running = True
+while running:
+    window.update()
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_1 += 1
+        pen.clear()
+        pen.write("Player 1: {} player 2: {}".format(score_1, score_2), align="center",
+                  font=("Courier", 24, "normal"))
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_2 += 1
+        pen.clear()
+        pen.write("Player 1: {} player 2: {}".format(score_1, score_2), align="center",
+                  font=("Courier", 24, "normal"))
+
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_2.ycor() + 50 and ball.ycor() > paddle_2.ycor() - 50):
+        ball.setx(340)
+        ball.dx *= -1
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_1.ycor() + 50 and ball.ycor() > paddle_1.ycor() - 50):
+        ball.setx(-340)
+        ball.dx *= -1
